688407f50a91b505d1acea8061e543f989a827ce
Cappeloow <cappe1996@hotmail.com>
2024-08-07 11:07:40 +0200

    feat:() Display usernames with comments on stocks

--- api/Mappers/CommentMapper.cs
+++ api/Mappers/CommentMapper.cs
@@ -15,6 +15,7 @@ namespace api.Mappers
                 Title = commentModel.Title,
                 Content = commentModel.Content,
                 CreatedAt = commentModel.CreatedAt,
+                CreatedBy = commentModel.AppUser.UserName,
                 StockId = commentModel.StockId
             };
         }
